58654cda015c0540a65ae374a95daec82d78bfe7
Dominic Cuneo <cuneo94@gmail.com>
2016-01-26 16:59:49 -0700

    added persona fidge gunkhaus

---
+++ public_html/epic/personas/Fidge_Gunkhaus.php
@@ -0,0 +1,24 @@
+
+/**
+ * Created by PhpStorm.
+ * User: OldManVin
+ * Date: 1/26/2016
+ * Time: 12:51 PM
+ */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8"/> <!-- the head is for meta data -->
+	<title> My web page </title>
+	</head>
+	<body>
+	<h1>Fidge Gunkhaus<h1>
+	<ul> <li>Male</li>
+		<li> age 37</li>
+		<li> white </li>
+		<li> He is a assistant distric attorny </li>
+		<li> Married 14 years </li>
+		<li>He has 2 kids named Ellie and chalupa batman</li>
+	</ul>
+	</body>
+	</html>
