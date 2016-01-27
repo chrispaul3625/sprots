b89142d530356c8d077c29da8bddbcaefaaa24a9
Dominic Cuneo <cuneo94@gmail.com>
2016-01-27 13:52:37 -0700

    fixed some text issues with Fidge persona

--- public_html/epic/personas/Fidge_Gunkhaus.php
+++ public_html/epic/personas/Fidge_Gunkhaus.php
@@ -1,10 +1,3 @@
-
-/**
- * Created by PhpStorm.
- * User: OldManVin
- * Date: 1/26/2016
- * Time: 12:51 PM
- */
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -14,7 +7,7 @@
 	<body>
 		<h3>Persona</h3>
 
-		<h2>Gunkhaus</h2>
+		<h2> Fidge Gunkhaus</h2>
 	<p> Fidge Gunkhaus is a 37 year old whiteman he is an assitant District attorney. He is married with two kids he uses his phone and computers constantly. He has a work laptop that he takes everywhere and another one at home. Huge football fan he watches every week and he is the commissioner of his fantasy football league. He is obsessed with fantasy football and so is the rest of  his league.He needs access to information about his lineup instantly even if he is at work or home or  on a vacation.</p>
 	</body>
 	</html>
