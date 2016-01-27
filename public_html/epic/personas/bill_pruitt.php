f6bb4cc3853e28cb17b1cc4625450d22ec29b798
Michael Prinz <mnprinz@gmail.com>
2016-01-27 13:57:41 -0700

    created persona file

---
+++ public_html/epic/personas/bill_pruitt.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+	<html lang="en"/>
+		<head>
+			<meta charset="utf-8"/>
+
+			<title>User Persona</title>
+		</head>
+	<body>
+		<h1>Persona</h1>
+		<p>
+			Bill Pruitt live in Minneapolis, MN and is 37 years old.  He is a married and has two children.
+			Bill grew up in Minneapolis and has a large extended family in the area.  He and his family are all big fans of the Minnesota Vikings football team, Minneapolis Twins baseball team and the Minnesota Wild hockey team.
+			Bill graduated from college and works for a bank in downtown Minneapolis
+
+		</p>
+	</body>
+</html>
