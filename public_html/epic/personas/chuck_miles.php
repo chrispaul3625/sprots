efdd7c0554224206dc9dfaa4ff575739dfe485ea
Chris Paul <chrispaul3625@gmail.com>
2016-01-27 13:46:25 -0700

    created Chuck Miles

---
+++ public_html/epic/personas/chuck_miles.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<meta charset="utf-8"/>
+
+		<h1>PERSONA</h1>
+	</head>
+	<body>
+		<h2>Chuck Miles</h2>
+		<p>
+			Chuck Miles is an average guy who is just getting into playing fantasy football with his coworkers. Chuck is
+			38 years old and hasn't really ever gotten into sports much. Chuck decides to get online and needs a place in which
+			he can review player stats, check out team information and also be able to save these teams to a profile so he can
+			easily monitor his teams and access player stats as well. Chuck primarily will be using his smartphone to check
+			these things.
+		</p>
+	</body>
+</html>created
