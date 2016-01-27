b2f8a5db538fac74352f1c1163df2564360ae321
Chris Paul <chrispaul3625@gmail.com>
2016-01-27 15:33:53 -0700

    Moved system goals to Executive Summary

--- public_html/epic/index.php
+++ public_html/epic/index.php
@@ -1,8 +1,8 @@
-<html>
-	<head>
+<!DOCTYPE html>
+	<html>
+		<head>
 		<meta charset="UTF-8"/>
-
-	</head>
+		</head>
 	<body>
 		<ul>
 			<li><a href="personas/travis-maher.php"> Travis Maher Persona.</a></li>
@@ -15,6 +15,16 @@
 			<li><a href="use-cases.php"> Use Cases.</a></li>
 			<li><a href="user-stories.php"> User Stories.</a></li>
 		</ul>
-
+		<h1> Executive Summary </h1>
+		<h1>SYSTEM GOALS</h1>
+		<p>
+			Sprots is going to be designed to give the team and individual players stats for these four professional teams NFL, NBA, MLB, and MLS.
+		</p>
+		<p>
+			The conceptual design is going to be one that will essentially reference outside libraries for the data, but provide it in a clear, easily searchable format.
+		</p>
+		<p>
+			To start, we'd like to make the searchable information absolutely free, to anyone. Going forward, and if plausible, We would like to implement a "push notification" style system.
+		</p>
 	</body>
 </html>
--- public_html/epic/system-goals.php
+++ public_html/epic/system-goals.php
@@ -1,17 +1,10 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8"/>
+		<title> System Goals</title>
 	</head>
-
 <body>
-		<p>
-		Sprots is going to be designed to give the team and individual players stats for these four professional teams NFL, NBA, MLB, and MLS.
-		</p>
-			<p>
-			The conceptual design is going to be one that will essentially reference outside libraries for the data, but provide it in a clear, easily searchable format.
-			</p>
-		<p>
-		To start, we'd like to make the searchable information absolutely free, to anyone. Going forward, and if plausible, We would like to implement a "push notification" style system.
-		</p>
+
 		</body>
 	</html>
