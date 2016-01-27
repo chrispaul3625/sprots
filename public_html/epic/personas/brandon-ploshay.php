12d77f588afbfec125a83a01d785f20480082b28
Dominic Cuneo <cuneo94@gmail.com>
2016-01-27 15:45:02 -0700

    renamed fidge gunkhaus and edited the persona

---
+++ public_html/epic/personas/brandon-ploshay.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8"/>
+	<title> capstone persona </title>
+	</head>
+	<body>
+		<h3>Persona</h3>
+		<h2> Brandon Ploshay</h2>
+	<p> Brandon Ploshay is a 37 year old man he is an assitant District attorney in Chicago. Born and raised in New York moved after graduating from New York Law school. He is married with 9 kids. He has a work laptop that he takes everywhere and another one at home both are a PC with windows 10. He has a Iphone 6 which he use constantly to check his fantasy team. He is the commissioner of his fantasy football league which he is obsessed with  and so is the rest of  his league. They watch games every week and compete for a trophy called the Tower of Pimps.He needs access to information instanly as to stay up to date and to plan his line up effecively aginst his leauge.  </p>
+	</body>
+	</html>
