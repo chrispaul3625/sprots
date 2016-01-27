4e5daf2517ae521702a31fa70da8ae4dd0cbe5ee
Domkratos <dmartinez337@cnm.edu>
2016-01-27 14:29:34 -0700

    added conceptual-model.php

--- public_html/epic/data-design/conceptual-model.php
+++ public_html/epic/data-design/conceptual-model.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dom
- * Date: 1/26/16
- * Time: 11:43 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Sprots Concepts</title>
+	</head>
+	<body>
+		<h1>
+			Conceptual Model for Sprots
+		</h1>
+		<p>
+			Sprots is going to be designed to give the team and individual players stats for these four professional teams NFL, NBA, MLB, and MLS.
+		</p>
+		<p>
+			The conceptual design is going to be one that will essentially reference outside libraries for the data, but provide it in a clear, easily searchable format.
+		</p>
+		<p>
+			To start, we'd like to make the searchable information absolutely free, to anyone. Going forward, and if plausible, We would like to implement a "push notification" style system.
+		</p>
+		<img src="Images/sprotsConceptualModel.svg" alt="Visual Conceptual Model">
+	</body>
+</html>
