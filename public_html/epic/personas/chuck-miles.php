1ef6f3388f3e7683be26b8fa5a753421f78a6069
Chris Paul <chrispaul3625@gmail.com>
2016-01-27 14:56:39 -0700

    Edit to personas

--- public_html/epic/personas/chuck-miles.php
+++ public_html/epic/personas/chuck-miles.php
@@ -10,7 +10,7 @@
 			Chuck Miles is an average guy who is just getting into playing fantasy football with his coworkers. Chuck is
 			38 years old and hasn't really ever gotten into sports much. Chuck decides to get online and needs a place in which
 			he can review player stats, check out team information and also be able to save these teams to a profile so he can
-			easily monitor his teams and access player stats as well. Chuck primarily will be using his smartphone to check
+			easily monitor his teams and access player stats as well. Chuck primarily will be using his Iphon 6s to check
 			these things.
 		</p>
 	</body>
--- public_html/epic/personas/travis-maher.php
+++ public_html/epic/personas/travis-maher.php
@@ -8,8 +8,8 @@
 		<h2>Travis Maher</h2>
 		<p>
 			Travis Maher is a soccer player who follows both soccer and football very closely. Travis has been into sports
-			his whole life. He is 29 years old and lives in Denver. He works IT but so he has ample access to a computer
-			but also uses a smartphone a ton of the time as well. Travis wants to be able to check the stats of his favorite
+			his whole life. He is 29 years old and lives in Denver. He works IT but so he has ample access to a HP running windows 10
+			but also uses an Iphone 6s a ton of the time as well. Travis wants to be able to check the stats of his favorite
 			football and soccer teams, while also being able to access player stats as well. Travis prefers to do this via his
 			smartphone but does check these things on his computer at work sometimes as well. Travis would like to be able
 			to check these things quickly and effiecently over his mobile device for the most part however. Travis would like
