cb4d6be2b2a0304050121db9d627eddc579136ea
Chris Paul <chrispaul3625@gmail.com>
2016-01-26 20:04:28 -0700

    Corrected some grammatical errors

--- public_html/epic/personas/travis_maher.php
+++ public_html/epic/personas/travis_maher.php
@@ -10,7 +10,7 @@
 			Travis Maher is a soccer player who follows both soccer and football very closely. Travis has been into sports
 			his whole life. He is 29 years old and lives in Denver. He works IT but so he has ample access to a computer
 			but also uses a smartphone a ton of the time as well. Travis wants to be able to check the stats of his favorite
-			football and soccer teams, while also be able to access player stats as well. Travis prefers to do this via his
+			football and soccer teams, while also being able to access player stats as well. Travis prefers to do this via his
 			smartphone but does check these things on his computer at work sometimes as well. Travis would like to be able
 			to check these things quickly and effiecently over his mobile device for the most part however. Travis would like
 			to be able to save his favorite teams and be able to check scores and stats of just those teams.
