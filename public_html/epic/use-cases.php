247f06e3b50675fd03c036adeaa2fc2a2e95b1b7
Dominic Cuneo <cuneo94@gmail.com>
2016-01-27 16:11:18 -0700

    replaceed fidge with brandon and put in use case

--- public_html/epic/use-cases.php
+++ public_html/epic/use-cases.php
@@ -13,6 +13,13 @@
 						<li>Bill sees content about his team that interests him and he clicks on the link.</li>
 						<li>The site brings up the content about the team</li>
 					</ul>
+		<h3> Brandon Ploshay </h3>
+		<ul>
+			<li>Brandon needs instant  access on all his devices iphone 10 and PC with windows 10 </li>
+			<li>Needs accurate stats for every player and team.</li>
+			<li>Notifications about player injures and game wins/loses </li>
+			<li>Needs to have a list of his players and team.</li>
+		</ul>
 	</body>
 
 </html>
