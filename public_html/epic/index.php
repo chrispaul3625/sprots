6dc2ff60e62727e6050574b57149378bdef0ff8f
Chris Paul <chrispaul3625@gmail.com>
2016-01-27 14:41:17 -0700

    Links set to Index.php

---
+++ public_html/epic/index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<ul>
+		<li><a href="personas/travis-maher.php"> Travis Maher Persona.</a></li>
+		<li><a href="personas/jude-chavez.php"> Jude Chavez Persona. </a>
+		<li><a href="personas/fidge-gunkhaus.php"> Fidge Gunkhaus Persona.</a></li>
+		<li><a href="personas/chuck-miles.php"> Chuck Miles Persona.</a></li>
+		<li><a href="data-design/conceptual-model.php"> Conceptual Model.</a></li>
+		<li><a href="data-design/erd.php"> ERD Model.</a></li>
+		<li><a href="system-goals.php"> System Goals.</a></li>
+		<li><a href="use-cases.php"> Use Cases.</a></li>
+		<li><a href="user-stories.php"> User Stories.</a></li>
+			</ul>
+	</head>
+	<body>
+
+
+	</body>
+</html>
