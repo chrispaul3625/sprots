cc5c62cb27ee320a2a7afbb0d3e343637029560b
Jude Chavez <Chavezjude7@gmail.com>
2016-01-27 13:59:59 -0800

    added person php

---
+++ public_html/epic/personas/jude_chavez.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Jude Chavez Persona</title>
+	</head>
+	<body>
+		<h1>Jude Chavez</h1>
+		<ul>
+			<li>Male</li>
+			<li>Age: 19</li>
+			<li>Nurse</li>
+			<li>Single</li>
+			<li>Spends large amount of time with friends and in social situations.</li>
+		</ul>
+		<p>
+			Jude is a 19 year old male who works as nurse. Jude does not follow sports very closesly. Jude does not have<br>
+			a huge knowledge base of sports stats and players on different teams. Jude however, wants to be able to keep up<br>
+			with individuals having conversations about sports and wants to be able look informed despite no following<br>
+			the sports in question. Jude wishes to keep track of the main sports being talked about (football, basketball, and<br>
+			soccer). Jude want to be able to quickly check stats of a certain team and sport quickly enough to be able to keep<br>
+			up with any conversation. Jude will use the site both on his smart phone and on his desktop.
+		</p>
+	</body>
+</html>
