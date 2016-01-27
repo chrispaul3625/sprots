b9c5e38b719dc25cbb13883c9091724f985c4d06
Michael Prinz <mnprinz@gmail.com>
2016-01-27 14:28:27 -0700

    created persona file

--- public_html/epic/personas/bill_pruitt.php
+++ public_html/epic/personas/bill_pruitt.php
@@ -10,8 +10,17 @@
 		<p>
 			Bill Pruitt live in Minneapolis, MN and is 37 years old.  He is a married and has two children.
 			Bill grew up in Minneapolis and has a large extended family in the area.  He and his family are all big fans of the Minnesota Vikings football team, Minneapolis Twins baseball team and the Minnesota Wild hockey team.
-			Bill graduated from college and works for a bank in downtown Minneapolis
-
+			Bill graduated from college and works for a bank in downtown Minneapolis.  His work and family resposibilities take most of his time but tries to attends as many games as he can and enjoys taking about his teams with his friends, family and co-workers.
+			Bill owns an iPhone 6 and a desktop PC and uses them both to access information about his favorite teams. He follows a few sports blogs, reads the sports pages of local and regional newspapers and uses social media sites to discuss sports with his friends.
+		</p>
+		<h2>Use Case</h2>
+		<p>Bill wants to get the latest news about his favorite teams.
+			Bill wants to be able to comment about the latest news concerning his favorite teams.
+			Bill wants to upvote or downvote news about his teams.
+			Bill wants to get information about his teams statistics as compared to other teams in the league.
+			Bill wants to get statistics about particular players of the teams he follows.
+			Bill wants to quickly and easily navigate the site to access all this information.
+			Bill wants links to team websites or links to articles about his teams.
 		</p>
 	</body>
 </html>
