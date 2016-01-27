9d91c271688fbcbe34ac5b5349e195b5cb87b0e5
Dominic Cuneo <cuneo94@gmail.com>
2016-01-27 15:46:29 -0700

    renamed fidge gunkhaus and edited the persona

--- public_html/epic/use-cases.php
+++ public_html/epic/use-cases.php
@@ -13,8 +13,6 @@
 						<li>Bill sees content about his team that interests him and he clicks on the link.</li>
 						<li>The site brings up the content about the team</li>
 					</ul>
-				<p> Fidge Gunkhaus will primarily be using his phone to access the site. Fide will create a profile so he can have the most up to the moment info about the memebers of his line up.</p>
-				<p>Fidge will create a profile to access the premium services. He will click on the sign up button creating a menu for his firstname,lastname,  email, billing address, mailing address,card information </p>
 	</body>
 
 </html>
