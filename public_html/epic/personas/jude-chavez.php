6b995241b22ee6c1ec0b4a5628609038dadbd40d
Jude Chavez <Chavezjude7@gmail.com>
2016-01-27 14:57:13 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	public_html/epic/personas/jude_chavez.php

--- public_html/epic/personas/jude-chavez.php
+++ public_html/epic/personas/jude-chavez.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+<!--
+*created by phpstorm
+* User: jchavez790
+* Date: 1/27/2016
+* Time: 1:40 PM
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -14,12 +22,14 @@
 			<li>Spends large amount of time with friends and in social situations.</li>
 		</ul>
 		<p>
-			Jude is a 19 year old male who works as nurse. Jude does not follow sports very closely. Jude does not have
-			a huge knowledge base of sports stats and players on different teams. Jude however, wants to be able to keep up
-			with individuals having conversations about sports and wants to be able look informed despite no following
-			the sports in question. Jude wishes to keep track of the main sports being talked about (football, basketball, and
-			soccer). Jude want to be able to quickly check stats of a certain team and sport quickly enough to be able to keep
+			Jude is a 19 year old male who works as nurse. Jude does not follow sports very closesly. Jude does not have<br>
+			a huge knowledge base of sports stats and players on different teams. Jude however, wants to be able to keep up<br>
+			with individuals having conversations about sports and wants to be able look informed despite no following<br>
+			the sports in question. Jude wishes to keep track of the main sports being talked about (football, basketball, and<br>
+			soccer). Jude want to be able to quickly check stats of a certain team and sport quickly enough to be able to keep<br>
 			up with any conversation. Jude will use the site both on his smart phone and on his desktop.
 		</p>
 	</body>
 </html>
+=======
+>>>>>>> origin/master
