d76c518ec71616dd2e3e464cade02f9046c16904
Jude Chavez <Chavezjude7@gmail.com>
2016-01-27 15:36:16 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	public_html/epic/personas/jude_chavez.php

--- public_html/epic/personas/jude-chavez.php
+++ public_html/epic/personas/jude-chavez.php
@@ -22,11 +22,11 @@
 			<li>Spends large amount of time with friends and in social situations.</li>
 		</ul>
 		<p>
-			Jude is a 19 year old male who works as nurse. Jude does not follow sports very closesly. Jude does not have<br>
-			a huge knowledge base of sports stats and players on different teams. Jude however, wants to be able to keep up<br>
-			with individuals having conversations about sports and wants to be able look informed despite no following<br>
-			the sports in question. Jude wishes to keep track of the main sports being talked about (football, basketball, and<br>
-			soccer). Jude want to be able to quickly check stats of a certain team and sport quickly enough to be able to keep<br>
+			Jude is a 19 year old male who works as nurse. Jude does not follow sports very closesly. Jude does not have
+			a huge knowledge base of sports stats and players on different teams. Jude however, wants to be able to keep up
+			with individuals having conversations about sports and wants to be able look informed despite no following
+			the sports in question. Jude wishes to keep track of the main sports being talked about (football, basketball, and
+			soccer). Jude want to be able to quickly check stats of a certain team and sport quickly enough to be able to keep
 			up with any conversation. Jude will use the site both on his smart phone and on his desktop.
 		</p>
 	</body>
