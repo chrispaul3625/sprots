9f8c48f1ad013c6284fda44d6c6b4e1bb13ed95f
Dominic Cuneo <cuneo94@gmail.com>
2016-01-27 13:48:39 -0700

    Added user story file

---
+++ public_html/epic/personas/user_stories.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/> <!-- the head is for meta data -->
+		<title>User story page </title>
+	</head>
+	<body>
+		<h1>User Story </h1>
+		<ul>
+			<li> The user needs to be able to create a fantasy lineup and change it accordingly </li>
+			<li> Should get updates about his or her lineup as we get the information. </li>
+			<li> Can tell them who in their lineup is playing what team. </li>
+			<li> Will keep the user updated about the scores and injures during games. </li>
+			<li> Can comapre show score between different linups</li>
+
+			<li> As a user I want to be able to look at stats of players and my favorite teams. </li>
+			<li> As an administrator I want to be able to suspend an account.</li>
+			<li> As a user I want to be able to save favortie teams to my profile and view stats of those teams.</li>
+			<li> As a user I want to be able to remove favorite teams from my profile. </li>
+			<li> As a user I want to be able to search and look at stats for all teams in the NFL, MLB, MLS, and NBA.</li>
+			<li> As a user I want to be able to look at upcomming fixtures for teams in the NFL, MLB, MLS, and NBA. </li>
+		</ul>
+	</body
+
+</html>
