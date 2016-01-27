ad4d69c122e99b6b218d4a240d7fd231709f5b0b
Dominic Cuneo <cuneo94@gmail.com>
2016-01-27 14:48:01 -0700

    started on use cases

---
+++ public_html/epic/use-cases.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title> Use cases page</title>
+	</head>
+	<body>
+		<h1>Use cases </h1>
+				<p> Fidge Gunkhaus will primarily be using his phone to access the site. Fide will create a profile so he can have the most up to the moment info about the memebers of his line up.</p>
+				<p>Fidge will create a profile to access the premium services. He will click on the sign up button creating a menu for his firstname,lastname,  email, billing address, mailing address,card information </p>
+	</body
+
+</html>
