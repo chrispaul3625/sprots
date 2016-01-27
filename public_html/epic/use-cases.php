b89c110e22936892e66e7c59943291890077ffb8
Michael Prinz <mnprinz@gmail.com>
2016-01-27 15:34:15 -0700

    feedback from scrum

--- public_html/epic/use-cases.php
+++ public_html/epic/use-cases.php
@@ -6,8 +6,15 @@
 	</head>
 	<body>
 		<h1>Use cases </h1>
+				<h2>Bill Pruitt</h2>
+					<ul>
+						<li>Bill accesses the site on his iPhone6.</li>
+						<li>Bill has already configured his profile to bring up the content for the teams he follows, i.e. Viking, Twins.</li>
+						<li>Bill sees content about his team that interests him and he clicks on the link.</li>
+						<li>The site brings up the content about the team</li>
+					</ul>
 				<p> Fidge Gunkhaus will primarily be using his phone to access the site. Fide will create a profile so he can have the most up to the moment info about the memebers of his line up.</p>
 				<p>Fidge will create a profile to access the premium services. He will click on the sign up button creating a menu for his firstname,lastname,  email, billing address, mailing address,card information </p>
-	</body
+	</body>
 
 </html>
