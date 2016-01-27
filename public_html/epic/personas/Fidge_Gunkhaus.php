21ff6898e59e0ca951445d0e350fe183e883cbf3
Chris Paul <chrispaul3625@gmail.com>
2016-01-27 13:50:41 -0700

    Edited personas

--- public_html/epic/personas/Fidge_Gunkhaus.php
+++ public_html/epic/personas/Fidge_Gunkhaus.php
@@ -16,14 +16,5 @@
 
 		<h2>Gunkhaus</h2>
 	<p> Fidge Gunkhaus is a 37 year old whiteman he is an assitant District attorney. He is married with two kids he uses his phone and computers constantly. He has a work laptop that he takes everywhere and another one at home. Huge football fan he watches every week and he is the commissioner of his fantasy football league. He is obsessed with fantasy football and so is the rest of  his league.He needs access to information about his lineup instantly even if he is at work or home or  on a vacation.</p>
-		<h3>User Story </h3>
-		<ul>
-			<li> The user needs to be able to create a fantasy lineup and change it accordingly </li>
-			<li> Should get updates about his or her lineup as we get the information. </li>
-			<li> Can tell them who in their lineup is playing what team. </li>
-			<li> Will keep the user updated about the scores and injures during games. </li>
-			<li> Can comapre show score between different linups</li>
-		</ul>
-
 	</body>
 	</html>
--- public_html/epic/personas/travis_maher.php
+++ public_html/epic/personas/travis_maher.php
@@ -15,18 +15,5 @@
 			to check these things quickly and effiecently over his mobile device for the most part however. Travis would like
 			to be able to save his favorite teams and be able to check scores and stats of just those teams.
 		</p>
-		<h3>
-			USER STORY
-		</h3>
-		<p>
-			<ul>
-			<li> As a user I want to be able to look at stats of players and my favorite teams. </li>
-			<li> As an administrator I want to be able to suspend an account.</li>
-			<li> As a user I want to be able to save favortie teams to my profile and view stats of those teams.</li>
-			<li> As a user I want to be able to remove favorite teams from my profile. </li>
-			<li> As a user I want to be able to search and look at stats for all teams in the NFL, MLB, MLS, and NBA.</li>
-			<li> As a user I want to be able to look at upcomming fixtures for teams in the NFL, MLB, MLS, and NBA. </li>
-		</ul>
-		</p>
 	</body>
 </html>
