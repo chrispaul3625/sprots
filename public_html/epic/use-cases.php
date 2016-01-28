ff6f48105bd78b8ec694aeb79a7783e2ef238d00
Dominic Cuneo <cuneo94@gmail.com>
2016-01-28 13:16:54 -0700

    edited use case

--- public_html/epic/use-cases.php
+++ public_html/epic/use-cases.php
@@ -13,11 +13,13 @@
 						<li>Bill sees content about his team that interests him and he clicks on the link.</li>
 						<li>The site brings up the content about the team</li>
 					</ul>
-				<h2>Fidge Gunkhaus</H2>
-						<li>Fidge accesses the site on his android phone.</li>
-						<li>Fidge will have a profile that will send him allow him to see up to date information about the teams he follows, i.e. Vikings, Twins.</li>
-						<li>Fidge will create a profile to access premium information by clicking the "sign up" button, which will create a form for him to enter his firstname, lastname, email, billing address, mailing address,and card
-							information.</li>
+				<h2>Brandon Ploshay</h2>
+					<ul>
+						<li>Brandon accesses the site on his iphone.</li>
+						<li>Brandon will have a profile that will send him allow him to see up to date information about his fantasy line up</li>
+						<li>Notifiactions tell Brandon about injuries and win/loses.</li>
+						<li>Brandon has a premium profile  that has  teh most up to the minute information.</li>
+					</ul>
 				<h2>Jude Chavez</h2>
 					<ul>
 						<li>Jude accesses the site on his work computer that runs IE.</li>
