2f2b04df283955973758817b304f1804ee73210f
Domkratos <dmartinez337@cnm.edu>
2016-01-29 22:45:55 -0700

    added in new erd design, done by Mike

--- public_html/epic/data-design/erd.php
+++ public_html/epic/data-design/erd.php
@@ -8,6 +8,6 @@
 		<header>
 			Entity Relationship Diagram
 		</header>
-		<img src="images/sprotsErd.svg" alt="sprots erd dun gone">
+		<img src="images/sprotStatErd.svg" alt="sprots erd dun gone">
 	</body>
 </html>
