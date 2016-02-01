8f0f0123d5d7775b73712581929950da5cf8602d
Chris Paul <chrispaul3625@gmail.com>
2016-02-01 16:13:30 -0700

    Revised user stories and removed Personas that we dont need anymore.

--- public_html/epic/use-cases.php
+++ public_html/epic/use-cases.php
@@ -13,13 +13,6 @@
 						<li>Bill sees content about his team that interests him and he clicks on the link.</li>
 						<li>The site brings up the content about the team</li>
 					</ul>
-				<h2>Brandon Ploshay</h2>
-					<ul>
-						<li>Brandon accesses the site on his iphone.</li>
-						<li>Brandon will have a profile that will send him allow him to see up to date information about his fantasy line up</li>
-						<li>Notifications tell Brandon about injuries and win/loses.</li>
-						<li>Brandon has a premium profile that has the most up to the minute information.</li>
-					</ul>
 				<h2>Jude Chavez</h2>
 					<ul>
 						<li>Jude accesses the site on his work computer that runs IE.</li>
--- public_html/epic/user-stories.php
+++ public_html/epic/user-stories.php
@@ -7,11 +7,9 @@
 	<body>
 		<h1>User Story </h1>
 		<ul>
-			<li> A registered user needs to be able to create a fantasy lineup and change it accordingly </li>
-			<li> A registered user should get updates about his or her lineup as we get the information. </li>
-			<li> A registered can tell them who in their lineup is playing what team. </li>
+			<li> A registered user should get updates about his or her favorites list as we get the information. </li>
+			<li> A registered can tell them who in their favorites list is playing what team. </li>
 			<li> A keep the user updated about the scores and injures during games registered will get notifications. </li>
-			<li> A registered Can comapre show score between different linups</li>
 
 			<li> As a registered user I want to be able to look at stats of players and my favorite teams. </li>
 			<li> As a registered user I want to be able to save favortie teams to my profile and view stats of those teams.</li>
