64dfee6be566476b4ce32c8c87c106c2ddd6a08d
Chris Paul <chrispaul3625@gmail.com>
2016-02-01 16:10:44 -0700

    Updated stats that we will keep to use in our project that is limited by the APIs, Removed the fantasy portions of the sports.

--- public_html/epic/data-design/stats.php
+++ public_html/epic/data-design/stats.php
@@ -1,74 +1,53 @@
 <!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="UTF-8">
-			<title>
-				Sport Stats
-			</title>
-		</head>
-		<body>
-			<h1> Sport Stats being used.</h1>
-			<h2>Soccer Stats</h2>
-				<ul>
-				<li> Live Score Feed of the EPL Live Matches.</li>
-				<li> Live Commentary on important matches including events, line-ups, substitutions and others.</li>
-				<li> Standing table of the league.</li>
-				<li> Fixtures for the current day and for the next and past 7 days.</li>
-			</ul>
-			<h2> Football </h2>
-			<ul>
-				<li> Schedules </li>
-				<li> Scores </li>
-				<li> Odds</li>
-				<li> Standings</li>
-				<li> Rosters </li>
-				<li> Box Scores </li>
-				<li> Team Stats </li>
-				<li> Player Stats </li>
-				<li> Projected Stats </li>
-				<li> Depth Charts </li>
-				<li> Injuries </li>
-				<li> Inactives </li>
-				<li> Snap Counts </li>
-				<li> News </li>
-				<li> Fantasy Points </li>
-				<li> Projected Fantasy Points </li>
-				<li> Fantasy D/ST Stats </li>
-			</ul>
-			<h2> Baseball </h2>
-				<ul>
-				<li> Schedules </li>
-				<li> Scores </li>
-				<li> Pitch by pitch </li>
-				<li> Odds</li>
-				<li> Standings</li>
-				<li> Rosters </li>
-				<li> Box Scores </li>
-				<li> Team Stats </li>
-				<li> Player Stats </li>
-				<li> Projected Stats </li>
-				<li> Probable pitchers </li>
-				<li> Starting lineups </li>
-				<li> Injuries </li>
-				<li> News </li>
-				<li> Fantasy Points </li>
-				<li> Projected Fantasy Points </li>
-			</ul>
-			<h2> Basketball </h2>
-				<ul>
-				<li> Schedule </li>
-				<li> Scores </li>
-				<li> Odds</li>
-				<li> Standings</li>
-				<li> Rosters </li>
-				<li> Box Scores </li>
-				<li> Team Stats </li>
-				<li> Player Stats </li>
-				<li> Projected Stats </li>
-				<li> Injuries </li>
-				<li> News </li>
-				<li> Fantasy Points </li>
-				<li> Projected Fantasy Points </li>
-			</ul>
-		</body>
-	</html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			Sport Stats
+		</title>
+	</head>
+	<body>
+		<h1> Sport Stats being used.</h1>
+		<h2>Soccer Stats</h2>
+		<ul>
+			<li> Live Score Feed of the EPL Live Matches.</li>
+			<li> Live Commentary on important matches including events, line-ups, substitutions and others.</li>
+			<li> Standing table of the league.</li>
+			<li> Fixtures for the current day and for the next and past 7 days.</li>
+		</ul>
+		<h2> Football </h2>
+		<ul>
+			<li> Schedules </li>
+			<li> Scores </li>
+			<li> Standings</li>
+			<li> Rosters </li>
+			<li> Team Stats </li>
+			<li> Player Stats </li>
+			<li> Depth Charts </li>
+			<li> Injuries </li>
+			<li> Inactives </li>
+		</ul>
+		<h2> Baseball </h2>
+		<ul>
+			<li> Schedules </li>
+			<li> Scores </li>
+			<li> Standings</li>
+			<li> Rosters </li>
+			<li> Team Stats </li>
+			<li> Player Stats </li>
+			<li> Probable pitchers </li>
+			<li> Starting lineups </li>
+			<li> Injuries </li>
+		</ul>
+		<h2> Basketball </h2>
+		<ul>
+			<li> Schedule </li>
+			<li> Scores </li>
+			<li> Standings</li>
+			<li> Rosters </li>
+			<li> Team Stats </li>
+			<li> Player Stats </li>
+			<li> Injuries </li>
+		</ul>
+	</body>
+</html>
