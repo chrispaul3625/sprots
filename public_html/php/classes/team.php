6fd6b5cebaa2bdc6eeec3932f26a3db7b96ea473
Chris Paul <chrispaul3625@gmail.com>
2016-02-02 14:05:11 -0700

    Created Constructor for Team
    https://app.asana.com/0/84113659891085/85573252999751

--- public_html/php/classes/team.php
+++ public_html/php/classes/team.php
@@ -1,10 +1,53 @@
 <?php
+
 /**
  * A team, This will be a team that is being monitored by our stats and participates in competing with other teams.
  *
  * @author Chris Paul <chrispaul3625@gmail.com>
-*/
+ */
+class team {
+	/**
+	 * id for this team; this is the primary key
+	 * @var int $teamId ;
+	 */
+	private $teamId;
+	/**
+	 * teamCity, one city per team.
+	 * @var string $teamCity ;
+	 */
+	private $teamCity;
+	/**
+	 * teamName, one team name per team.
+	 * @var string $teamName ;
+	 */
+	private $teamName;
+	/**
+	 * teamApiId, one Api id per team.
+	 * @var int $teamApiId ;
+	 */
+	private $teamApiId;
 
-	class team {
+	/**
+	 * Constructor for this team
+	 *
+	 * @param int $newTeamId id of the team that is being created
+	 * @param string $newTeamCity city associated with team
+	 * @param string $newTeamName name associated with team
+	 * @param int $newTeamApiId Api id that is associated with team
+	 */
+	public function __construct($newTeamId, $newTeamCity, $newTeamName, $newTeamApiId = null) {
+		$this->setTeamId($newTeamId);
+		$this->setTeamCity($newTeamCity);
+		$this->setTeamName($newTeamName);
+		$this->setTeamApiId($newTeamApiId);
+	} catch(InvalidArgumentException $invalidArgument) {
+throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+}
 
+catch
+(RangeException $range){
+	throw(new RangeException($range->getMessage(), 0, $range));
+} catch(Exception $exception){
+	throw(new Exception($exeption->getMessage(), 0, $exception));
+}
 	}
