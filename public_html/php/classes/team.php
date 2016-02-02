996ad36497d9fb1ec5405741846933b4cc256d21
Chris Paul <chrispaul3625@gmail.com>
2016-02-02 12:39:05 -0700

    Created directory fields to classes, created team and teamStatistics class.

---
+++ public_html/php/classes/team.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * A team, This will be a team that is being monitored by our stats and participates in competing with other teams.
+ *
+ * @author Chris Paul <chrispaul3625@gmail.com>
+*/
+
+	class team {
+
+	}
---
+++ public_html/php/classes/teamStatistic.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Team Statistic, This is a field in which all statistics related to a team are going to be held.
+ *
+ * @author Chris Paul <chrispaul3625@gmail.com>
+ */
+
+class teamStatistics {
+
+}
