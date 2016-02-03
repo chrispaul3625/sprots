cf06a3a4d066dcc7cec2965b0db88d2a88cc6527
Dominic Cuneo <cuneo94@gmail.com>
2016-02-03 14:01:23 -0700

    finished constructor https://app.asana.com/0/84113659891121/86650635041932

--- public_html/php/classes/Game.php
+++ public_html/php/classes/Game.php
@@ -17,7 +17,7 @@ require_once("autoloader.php");
  */
 class Game {
 	/**
-	 * id for gameid primary key
+	 * id for gameId primary key
 	 * @var int $gameId
 	 */
 	private $gameId;
@@ -33,7 +33,43 @@ class Game {
 	private $gameSecondId;
 	/**
 	 * id for gameTime
-	 * var $gameTime
+	 * var $gameTimeDate
 	 */
 	private $gameTime;
+	/**
+	 * constructor fo Game
+	 *
+	 * @param int|null $newGameId id of this Game or null if a new Tweet
+	 * @param int $newGameId for the last game played for a sport
+	 * @param int $newGameFirstTeamId id for the first team in a game
+	 * @param int $newGameSecondTeamId id  fo the second team in a game
+	 * @param \DateTime|string|null $newGameDate date and time for when game was played
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values exceed limits
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 */
+	public function  __construct(int $newGameId = null, int $newGameFirstTeamId, $newGameSecondTeamId, $newGameTimeDate = null) {
+		try{
+			$this->setgameId($newGameId);
+			$this->setgameFirstTeamId($newGameFirstTeamId);
+			$this->setgameSecondId($newGameSecondTeamId);
+			$this->setGameTimeDate($newGameTimeDate);
+		}catch(\InvalidArgumentException $invalidArgument){
+			//rethrow the exception to caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		}catch(\RangeException $range){
+			// rethrow
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}catch(\TypeError $typeError){
+			// rethrow
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		}catch (\Exception $exception){
+			// rethrow
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+	/**
+	 * accessor
+	 */
 }
