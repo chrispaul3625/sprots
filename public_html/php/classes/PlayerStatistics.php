f2f4642519ba255a6b77f634348b579338a906cf
Chris Paul <chrispaul3625@gmail.com>
2016-02-03 13:09:10 -0700

    foo
    https://app.asana.com/0/84113647261172/86650635041932

--- public_html/php/classes/PlayerStatistics.php
+++ public_html/php/classes/PlayerStatistics.php
@@ -8,23 +8,33 @@ require_once("autoloader.php");
  * @author Chris Paul <chrispaul3625@gmail.com>
  **/
 	class PlayerStatistics {
-	/**
-	 * $playerStatisticGameId id for player in a specific game; this is a foreign key
-	 * @var int $playerStatisticGameId
-	 **/
-	private $playerStatisticGameId;
-	/**
-	 * $playerStatisticPlayerId id for players overall statistics, this is a foreign key
-	 * @var int $playerStatisticPlayerId
-	 **/
-	private $playerStatisticPlayerId;
-	/**
-	 * $playerStatisticStatisticId id for the players individual statistic, this is a foreign key
-	 * @var int $playerStatisticStatisticId
-	 **/
-	private $playerStatisticStatisticId;
-	/**
-	 * , one Api id per team
-	 * @var int $playerStatisticValue
-	 **/
-	private $playerStatisticValue;
+		/**
+		 * $playerStatisticGameId id for player in a specific game; this is a foreign key
+		 * @var int $playerStatisticGameId
+		 **/
+		private $playerStatisticGameId;
+		/**
+		 * $playerStatisticPlayerId id for players overall statistics, this is a foreign key
+		 * @var int $playerStatisticPlayerId
+		 **/
+		private $playerStatisticPlayerId;
+		/**
+		 * $playerStatisticStatisticId id for the players individual statistic, this is a foreign key
+		 * @var int $playerStatisticStatisticId
+		 **/
+		private $playerStatisticStatisticId;
+		/**
+		 * $playerStatisticValue the value of individual stats, number value for a stat
+		 * @var int $playerStatisticValue
+		 **/
+		private $playerStatisticValue;
+
+
+
+
+
+
+
+
+
+	}
