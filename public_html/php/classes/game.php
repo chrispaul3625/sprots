e9354d5ec36d59b911e62f5abf8c1ffab6f7b964
Dominic Cuneo <cuneo94@gmail.com>
2016-02-03 11:25:38 -0700

    started class
    https://app.asana.com/0/84113659891121/86650635041932

--- public_html/php/classes/game.php
+++ public_html/php/classes/game.php
@@ -1,7 +1,22 @@
 <?php
+namespace Edu\Cnm\Dcuneo1\sprots;
+
+require_once ("autoLoader.php");
 /**
  * Created by PhpStorm.
  * User: OldManVin
  * Date: 2/2/2016
  * Time: 1:09 PM
  */
+
+/**
+ * game is the record of what teams played  each other and the scores
+ *
+ * @author Dominic Cuneo < cueno94@gmail.com
+ *
+ */
+class Game {
+	/**
+	 * id for gameid primary key
+	 */
+}
