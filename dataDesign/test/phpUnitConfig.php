5de07fe0e5233279b5b2138f7c835a867bcdeba7
Dom Kratos <dmartinez337@cnm.edu>
2016-02-07 13:50:54 -0700

    changed sql directory to dataDesign, and added in test directory, with phpUnitConfig file, for unit testing.

---
+++ dataDesign/test/phpUnitConfig.php
@@ -0,0 +1,21 @@
+<phpunit>
+	<testsuites>
+		<testsuite name="Sprots Data Design">
+			<file>FavoritePlayerTest.php</file>
+			<file>FavoriteTeamTest.php</file>
+			<file>GameTest.php</file>
+      <file>PlayerTest.php</file>
+      <file>PlayerStatisticsTest.php</file>
+      <file>ProfileTest.php</file>
+      <file>SportTest.php</file>
+      <file>StatisticTest.php</file>
+      <file>TeamTest.php</file>
+      <file>TeamStatisticTest.php</file>
+		</testsuite>
+	</testsuites>
+	<filter>
+		<whitelist processUncoveredFilesFromWhitelist="true">
+			<directory suffix=".php">../php/classes</directory>
+		</whitelist>
+	</filter>
+</phpunit>
