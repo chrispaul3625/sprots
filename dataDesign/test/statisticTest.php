5a872fbf544f86a9bac10f6b5ca68e437109e5aa
Dominic Cuneo <cuneo94@gmail.com>
2016-02-09 16:08:32 -0700

    started unitest https://app.asana.com/0/84113659891121/86650635041932

---
+++ dataDesign/test/statisticTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Edu\Cnm\Sprots;
+
+use Edu\Cnm\Sprots\{statistic};
+
+// grab the project test parameters
+require_once("phpUnitConfig.php");
+//grab the class under scrutiny
+require_once(dirname(__Statistic__) . "/php/classes/autoload.php");
+
+/**
+ * Full phpUnit test for statistic class
+ * This is
+ */
