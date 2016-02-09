52a74db5e192a10390660cb88cb2cb63c8511bb3
Dominic Cuneo <cuneo94@gmail.com>
2016-02-09 16:50:46 -0700

    con't unitest https://app.asana.com/0/84113659891121/85573252999755

--- dataDesign/test/statisticTest.php
+++ dataDesign/test/statisticTest.php
@@ -10,5 +10,21 @@ require_once(dirname(__Statistic__) . "/php/classes/autoload.php");
 
 /**
  * Full phpUnit test for statistic class
- * This is
+ * This is a complete phpunit test for the statistic class. It is complete because all my SQL/PDO enabled methods
+ * are tested fro both invalid and valid inputs
+ *
+ * @see Statistic
+ * @Dominic Cuneo <cuneo94@gmail.com
  */
+class StatisticTest extends DataDesign{
+	/**
+	 * content of Statistic
+	 * @var string $valid_statistic
+	 */
+	protected $VALID_STATISTIC = "PHPUnit test passing";
+	/**
+	 * content of the updated Statistic
+	 * @var string $VALID_STATISTIC2
+	 */
+	protected $VALID_STATISTIC2 = "PHPUnit test still passing";
+}
