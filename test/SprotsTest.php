687be7a1525f7dfac939c23801c8f04b6c52809e
Domkratos <dmartinez337@cnm.edu>
2016-02-12 11:34:22 -0700

    changed data design/dmcdonald to data-design/sprots

--- test/SprotsTest.php
+++ test/SprotsTest.php
@@ -88,8 +88,8 @@ abstract class SprotsTest extends \PHPUnit_Extensions_Database_TestCase {
 		// if the connection hasn't been established, create it
 		if($this->connection === null) {
 			// connect to mySQL and provide the interface to PHPUnit
-			$config = readConfig("/etc/apache2/data-design/dmcdonald21.ini");
-			$pdo = connectToEncryptedMySQL("/etc/apache2/data-design/dmcdonald21.ini");
+			$config = readConfig("/etc/apache2/data-design/sprots.ini");
+			$pdo = connectToEncryptedMySQL("/etc/apache2/data-design/sprots.ini");
 			$this->connection = $this->createDefaultDBConnection($pdo, $config["database"]);
 		}
 		return($this->connection);
