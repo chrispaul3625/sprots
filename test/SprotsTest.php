83144ac1b3e54f1e086fe3281eb67dc8d465a364
Chris Paul <chrispaul3625@gmail.com>
2016-02-12 12:20:48 -0700

    changed directory
    https://app.asana.com/0/84113647261172/88567781778144

--- test/SprotsTest.php
+++ test/SprotsTest.php
@@ -89,7 +89,7 @@ abstract class SprotsTest extends \PHPUnit_Extensions_Database_TestCase {
 		if($this->connection === null) {
 			// connect to mySQL and provide the interface to PHPUnit
 			$config = readConfig("/etc/apache2/capstone-mysql/sprots.ini");
-			$pdo = connectToEncryptedMySQL("/etc/capstone-mysql/sprots.ini");
+			$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/sprots.ini");
 			$this->connection = $this->createDefaultDBConnection($pdo, $config["database"]);
 		}
 		return($this->connection);
