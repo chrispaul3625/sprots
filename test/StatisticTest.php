2bbf9d888286a91969e52b8a76099246bd57f220
Dominic Cuneo <cuneo94@gmail.com>
2016-02-12 09:31:40 -0700

    updated with new state variable https://app.asana.com/0/84113659891121/86650635041932

--- test/StatisticTest.php
+++ test/StatisticTest.php
@@ -27,6 +27,11 @@ class StatisticTest extends Sprots {
 	 * @var string $VALID_STATISTIC2
 	 */
 	protected $VALID_STATISTIC2 = "PHPUnit test still passing";
+	/**
+	 * content of  Statistic
+	 * @var string $VALID_STATISTICNAME
+	 */
+	protected $VALID_STATISTICNAME = null;
 
 /**
  * test inserting a valid Statistic and verify that the actual mySQL data matches
@@ -42,7 +47,7 @@ class StatisticTest extends Sprots {
 		//grab the data from mySQL and enforce the fields match expectations
 		$pdoStatistic = Statistic::getStatisticByStatisticId($this->getPDO(), $statistic-getStatisticId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowcount("statistic"));
-		$this->assertEquals($pdoStatistic->getStatistic(), $this->VALID_STATISTIC);
+		$this->assertEquals($pdoStatistic->getStatistic(), $this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 	}
 	/**
 	 * test inserting statistic editing and update
@@ -51,7 +56,7 @@ class StatisticTest extends Sprots {
 		//count the number of rows and save
 		$numRows = $this->getConnection()->getRowCount("statistic");
 		//create a new Statistic and insert into mySQL
-		$statistic = new Statistic(null,$this->VALID_STATISTIC);
+		$statistic = new Statistic(null,$this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 		$statistic->insert($this->getPDO());
 
 		//edit statistic and update in mySQL
@@ -60,14 +65,14 @@ class StatisticTest extends Sprots {
 		//grab the data from mySQL and enforce the fields match expectations
 		$pdoStatistic = Statistic::getStatisticByStatisticId($this->getPDO(), $statistic-getStatisticId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowcount("statistic"));
-		$this->assertEquals($pdoStatistic->getStatistic(), $this->VALID_STATISTIC);
+		$this->assertEquals($pdoStatistic->getStatistic(), $this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 	}
 	/**
 	 * test updating a statistic that already exists
 	 */
 	public function testUpdateInvalidStatistic(){
 		//create a Statistic with a non null tweet id and watch it fail
-		$statistic = new Statistic(null, $this->VALID_STATISTIC);
+		$statistic = new Statistic(null, $this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 		$statistic->insert($this->getPDO());
 	}
 	/**
@@ -77,7 +82,7 @@ class StatisticTest extends Sprots {
 		//count the number of rows and save
 		$numRows = $this->getConnection()->getRowCount("statistic");
 		//create a new statistic adn insert into mySQL
-		$statistic = new Statistic(null, $this->VALID_STATISTIC);
+		$statistic = new Statistic(null, $this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 		$statistic->insert($this->getPDO());
 		//delete from mySQL
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("statistic"));
@@ -96,7 +101,7 @@ class StatisticTest extends Sprots {
 		//count the number of rows and save
 		$numRows = $this->getConnections()->getRowCount("statistic");
 		//create a new Statistic adn insert into mySQL
-		$statistic = new Statistic(null, $this->VALID_STATISTIC);
+		$statistic = new Statistic(null, $this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 		$this->insert($this->getPDO());
 		//grab data from mySQL and enforce  the fields to expectations
 
@@ -112,7 +117,7 @@ class StatisticTest extends Sprots {
 		$numRows = $this->getConnection()->getRowCount("statistic");
 
 		//create a new Game and insert into mySql
-		$game = new Game(null, $this->statistic->statisticId(), $this->VALID_STATISTIC);
+		$game = new Game(null, $this->statistic->statisticId(), $this->VALID_STATISTIC, $this->VALID_STATISTICNAME);
 		$game->insert($this->getPDO());
 
 		//grab the dat from mySQL and enforce the fields match
@@ -120,5 +125,8 @@ class StatisticTest extends Sprots {
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount('statistic'));
 		$this->assertCount(1, $results);
 		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Dcuneo1\\Sprots\\Statistic", $results);
+		// grab the result from the array and validate it
+		$pdoTweet = $results[0];
+		$this->assertEquals($pdoTweet->getStatisticName(), $this->VALID_STATISTICNAME);
 	}
 }
