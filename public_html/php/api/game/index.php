399920703e0cb6dee4b4dae5369505bcb654d50e
Dominic Cuneo <cuneo94@gmail.com>
2016-02-25 15:50:16 -0700

    started the game api https://app.asana.com/0/84113659891121/94393680990009

---
+++ public_html/php/api/game/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
+require_once dirname(dirname(__DIR__)) . "/lib./xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once (dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload");
+
+/**
+ * API for the game class
+ * @author Dominic Cuneo < cuneo94@gmail.com
+ */
+
+//verify the xsrf challange
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/sprots.ini");
+
+//determine which HTTP method was used
+$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $SERVER["REQUEST_METHOD"];
+
+//sanitize inputs
+$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+//make sure the id is valid for methods that require it
+if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)){
+	throw(new InvalidArgumentException("id can not be empty or negitive", 405 ));
+}
+
+//sanitize and trim other fields
+$
