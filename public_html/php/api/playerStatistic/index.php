2280adf3e59e4e6c7cd67672a3976805ec9d6f61
Chris Paul <chrispaul3625@gmail.com>
2016-02-25 13:22:17 -0700

    Created PlayerStatistic directory and index.php for the api setup
    https://app.asana.com/0/84113659891085/94393680990027

---
+++ public_html/php/api/playerStatistic/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload.php");
