2b4e1561ed9c573f84c6eb198eb26027e343a0cd
Chris Paul <chrispaul3625@gmail.com>
2016-02-26 11:12:28 -0700

    Added grab current directory to all front end pages
    https://app.asana.com/0/84113647261172/94393680990034

---
+++ public_html/baseballPlayerStatPage/index.php
@@ -0,0 +1,10 @@
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
+
+/*set page title here*/
+$PAGE_TITLE = "MY PAGE TITLE";
+
+/*load head-utils.php - edit path as needed*/
+require_once("php/templates/head-utils.php");
+?>
