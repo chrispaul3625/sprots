af419169192ed0c4b01ed2cc958113485a5e4820
Dominic Cuneo <cuneo94@gmail.com>
2016-02-26 09:35:21 -0700

    started the GET https://app.asana.com/0/84113659891121/94393680990009

--- public_html/php/api/game/index.php
+++ public_html/php/api/game/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
 require_once dirname(dirname(__DIR__)) . "/lib./xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
-require_once (dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload");
 
 /**
  * API for the game class
@@ -10,7 +10,7 @@ require_once (dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload");
  */
 
 //verify the xsrf challange
-if(session_status() !== PHP_SESSION_ACTIVE){
+if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
@@ -19,21 +19,40 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
-	// grab the mySQL connection
+try {
+// grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/sprots.ini");
 
 //determine which HTTP method was used
-$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $SERVER["REQUEST_METHOD"];
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $SERVER["REQUEST_METHOD"];
 
 //sanitize inputs
-$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 //make sure the id is valid for methods that require it
-if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)){
-	throw(new InvalidArgumentException("id can not be empty or negitive", 405 ));
-}
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id can not be empty or negitive", 405));
+	}
 
 //sanitize and trim other fields
-$gameId = filter_input(INPUT_GET, "gameId", FILTER_VALIDATE_INT);
-$gameFirstTeamId = filter_input(INPUT_GET, "gameFirstTeamId", FILTER_VALIDATE_INT);
-$gameSecondTeamId = filter_input(INPUT_GET, "gameSecondTeamID", FILTER_VALIDATE_INT);
-$gameTime = filter_input(INPUT_GET, "gameTime", FILTER_VALIDATE_DATE);
+	$gameId = filter_input(INPUT_GET, "gameId", FILTER_VALIDATE_INT);
+	$gameFirstTeamId = filter_input(INPUT_GET, "gameFirstTeamId", FILTER_VALIDATE_INT);
+	$gameSecondTeamId = filter_input(INPUT_GET, "gameSecondTeamID", FILTER_VALIDATE_INT);
+	$gameTime = filter_input(INPUT_GET, "gameTime", FILTER_VALIDATE_INT);
+	$gameTime /= 1000;
+	$gameTime = DateTime::createFromFormat("U", strval($gameTime));
+
+	// get the game based on the given field
+	if(empty($id) === false){
+		$game = Game::getGameByGameId($pdo, $id);
+		if($game !== null && $game->getGameId() === $_SESSION["game"]->getGameId()){
+			$reply->data = $game;
+		}
+	}
+
+
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+
+}
