1bde3c79128a2c29fad65bc0afc7e466179a1607
domkratos <dmartinez337@cnm.edu>
2016-02-26 19:31:51 -0700

    Finished up profile api, and moved lib directory to php directory.
    https://app.asana.com/0/84113659891049/94393680990019

--- public_html/php/api/profile/index.php
+++ public_html/php/api/profile/index.php
@@ -68,6 +68,7 @@ try {
 	}
 	// create, update, and delete of profile
 	if($method === "PUT") {
+		verifyXsrf();
 		$profile = Profile::getProfileId($pdo, $id);
 		if($profile === null) {
 			throw(new RuntimeException ("Profile does not exist"));
@@ -92,33 +93,107 @@ try {
 		if(($profile->getProfileId() === false) && ($requestObject->profilePassword !== null))
 			$profile->update($pdo);
 		$reply->message = "Profile Updated";
-	}
-	if($method === "POST") {
+	} // creating a profile
+	elseif($method === "POST") {
+
+		// hash the pw, and set it
+		$password = bin2hex(openssl_random_pseudo_bytes(32));
+		$salt = bin2hex(openssl_random_pseudo_bytes(32));
+		$hash = hash_pbkdf2("sha512", $password, $salt, 262144, 128);
+		$emailActivation = bin2hex(openssl_random_pseudo_bytes(8));
+		// create new profile
 		$profile = new Profile($id, $_SESSION["profile"]->getProfileId(), $requestObject->profileUserName, $requestObject->profleEmail, $hash, $salt);
 		$profile->insert($pdo);
-
 		$reply->message = "Profile Created!";
+		// compose and send the email for confirmation and setting a new password
+		// create swift message
+		$swiftMessage = Swift_Message::newInstance();
+		// attach the sender to the message
+		// this takes the form of an associative array where the Email is the key for the real name
+		$swiftMessage->setFrom(["email@gmail.com" => "Sprots"]);
+		/**
+		 * attach the recipients to the message
+		 * notice this an array that can include or omit the the recipient's real name
+		 * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
+		 **/
+		$recipients = [$requestObject->profileEmail];
+		$swiftMessage->setTo($recipients);
+		// attach the subject line to the message
+		$swiftMessage->setSubject("Please confirm your Sprots account");
+		/**
+		 * attach the actual message to the message
+		 * here, we set two versions of the message: the HTML formatted message and a special filter_var()ed
+		 * version of the message that generates a plain text version of the HTML content
+		 * notice one tactic used is to display the entire $confirmLink to plain text; this lets users
+		 * who aren't viewing HTML content in Emails still access your links
+		 **/
+		// building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm the account.
+		$basePath = $_SERVER["SCRIPT_NAME"];
+		for($i = 0; $i < 3; $i++) {
+			$lastSlash = strrpos($basePath, "/");
+			$basePath = substr($basePath, 0, $lastSlash);
+		}
+		$urlglue = $basePath . "/controllers/email-confirmation?emailActivation=" . $profile->getProfileEmail();
+		$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlglue;
+		$message = <<< EOF
+		<h1>You've been registered for Sprots, the best in up to date professional sport statistics!</h1>
+		<p>Visit the following URL to set a new password and complete the registration process: </p>
+		<a href="$confirmLink">$confirmLink</a></p>
+EOF;
+		$swiftMessage->setBody($message, "text/html");
+		$swiftMessage->addPart(html_entity_decode(filter_var($message, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)), "text/plain");
+		/**
+		 * send the Email via SMTP; the SMTP server here is configured to relay everything upstream via CNM
+		 * this default may or may not be available on all web hosts; consult their documentation/support for details
+		 * SwiftMailer supports many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+		 * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwitftMailer
+		 **/
+		$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+		$mailer = Swift_Mailer::newInstance($smtp);
+		$numSent = $mailer->send($swiftMessage, $failedRecipients);
+		/**
+		 * the send method returns the number of recipients that accepted the Email
+		 * so, if the number attempted is not the number accepted, this is an Exception
+		 **/
+		if($numSent !== count($recipients)) {
+			// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+			throw(new RuntimeException("unable to send email", 404));
+			/**
+			 * the send method returns the number of recipients that accepted the Email
+			 * so, if the number attempted is not the number accepted, this is an Exception
+			 **/
+			if($numSent !== count($recipients)) {
+				// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+				throw(new RuntimeException("unable to send email", 404));
+			}
+		}
 	} elseif($method === "DELETE") {
+		verifyXsrf();
 		// make sure the user is only deleting their own profile.
 		$security = Profile::getProfileByProfileId($pdo, $_SESSION["profile"]->getProfileId());
 		if(($security->getProfileId() === false) && ($_SESSION["profile"]->getProfileId() !== $profile->getProfileId())) {
 			$_SESSION["profile"]->setProfileId(false);
-
 			throw(new RunTimeException("You can only modify your own profile", 403));
 		}
-
 		$profile = Profile::getProfileByProfileId($pdo, $id);
 		if($profile === null) {
 			throw(new RangeException("Profile does not exist", 404));
 		}
-
 		$profile->delete($pdo);
 		$deleteObject = new stdClass();
 		$deleteObject->profileId = $id;
 		$reply->message = "Profile deleted.";
-	} catch(Exception $exception) {
+	}
+	// send exception bac to the caller
+} catch(Exception $exception) {
 		$reply->status = $exception->getCode();
 		$reply->message = $exception->getMessage();
 	}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
 }
+echo json_encode($reply);
+
+
 
