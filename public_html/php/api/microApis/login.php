3fcc045cf86c306e86be7f679ec6a328957eb5ae
domkratos <dmartinez337@cnm.edu>
2016-02-26 20:11:54 -0700

    Finished login micro api https://app.asana.com/0/84113659891049/94393680990030

--- public_html/php/api/microApis/login.php
+++ public_html/php/api/microApis/login.php
@@ -1,7 +1,60 @@
 <?php
+// auto loader
+require_once(dirname(dirname(dirname(__DIR__)) . "/php/classes/atoloder.php"));
+// imports xsrf
+require_once(dirname(dirname(__DIR__) . "/lib/xsrf.php"));
+// a security file that's on the schools server, that Dylan created, so it'll show not found.
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
 /**
- * Created by PhpStorm.
- * User: dom
+ *
+ * micro api for logging in
+ * @author Dom Kratos <dom@domkratos.com>
  * Date: 2/26/16
  * Time: 10:59 AM
  */
+
+// prepare the default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->message = null;
+
+
+try {
+	// start the session and create an xsrf token
+	if(session_start() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	verifyXsrf();
+
+	// grab the db connection
+	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/sprots.ini");
+
+	// convert JSON to an object
+	$requestContent = file_get_contents("php://input");
+	$requestObject = json_decode($requestContent);
+
+	// sanitize the email, and search by profileEmail
+	$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
+	$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
+
+	if($profile !== null) {
+		$profileHash = hash_pbkdf2("sha512", $requestObject->password, $profile->getProfileSalt(), 262144, 128);
+		if($profileHash === $profile->getProfileHash()) {
+			$_SESSION["profile"] = $profile;
+			$reply->status = 200;
+			$reply->message = "Successfully logged in";
+		} else {
+			throw(new InvalidArgumentException("email or password is invalid", 401));
+		}
+	}
+	// create an exception to pass back to the REST caller
+}  catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+
+
+header("Content-type: application/json");
+echo json_encode($reply);
+
