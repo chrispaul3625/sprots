538ff456c3c32493351ee05b24a97a87d078f6e5
Michael Prinz <mnprinz@gmail.com>
2016-02-29 16:39:05 -0700

    changed some shiz https://app.asana.com/0/84113659891109/94393680990021

--- public_html/php/api/Team/index.php
+++ public_html/php/api/Team/index.php
@@ -5,19 +5,48 @@
  * Date: 2/26/16
  * Time: 11:23 AM
  */
+require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-$query_params = array(
-	'Ocp-Apim-Subscription-key' => 'subkey',
-);
+//verify the xsrf challange
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
 
-$getdata = http_build_query($query_params);
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	$season = filter_input(INPUT_GET, "season", FILTER_SANITIZE_STRING);
+	if(empty($season) === true) {
+		$today = new DateTime();
+		$season = $today->format("Y");
+	}
+
+	$config = readConfig("/etc/apache2/capstone-mysql/sprots.ini");
 
 $opts = array(
 	'http' => array(
 		'method' => "GET",
-		'header' => "Content-Type: application/x-www-form-urlencoded", 'content' => $getdata)
+		'header' => "Content-Type: application/json\r\nOcp-Apim-Subscription-key: " . $config["fantasyData"], 'content' => "{body}")
 );
 $context = stream_context_create($opts);
 
-$response = file_get_contents('https://api.fantasydata.net/nfl/v2/JSON/Teams/season');
+$response = file_get_contents("https://api.fantasydata.net/nfl/v2/JSON/Teams/$season", false, $context);
+	$reply->data = json_decode($response);
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
 
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+echo json_encode($reply);
