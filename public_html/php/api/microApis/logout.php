79378e9da75d293dfb673c8b48cbcaae1a03a3a2
Domkratos <dmartinez337@cnm.edu>
2016-02-29 09:23:35 -0700

    made logut micro api/controller
    https://app.asana.com/0/84113647261172/94393680990030

--- public_html/php/api/microApis/logout.php
+++ public_html/php/api/microApis/logout.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: dom
+ * This is the micro api controller for a user to logout
+ * @author Dom Kratos <dom@domkratos.com>
  * Date: 2/26/16
  * Time: 11:09 AM
  */
+
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+unset($_SESSION["profile"]);
