b96a4b66b4427c2be3edd1aef2f8cd1d2f5c4e56
Chris Paul <chrispaul3625@gmail.com>
2016-03-09 13:43:28 -0700

    Updated the head libs file
    https://app.asana.com/0/84113647261172/94393680990034

--- public_html/php/templates/head-utils.php
+++ public_html/php/templates/head-utils.php
@@ -12,3 +12,34 @@ $ROOT_DEPTH = substr_count($ROOT_PATH, "/");
 $DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
 $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 ?>
+<!DOCTYPE html>
+<html lang="en" ng-app="NgTemplate">
+	<head>
+		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- set base for relative links - to enable pretty URLs. -->
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- Custom CSS  -->
+		<link rel="stylesheet" href="<?php echo $PREFIX;?>css/style.css" type="text/css"/>
+
+		<!--Angular JS-->
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.js"></script>
+
+		<!--Angular application files-->
+		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/app.js"></script>
+		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/main-controller.js"></script>
+		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/about-controller.js"></script>
+
+		<title><?php echo $PAGE_TITLE;?></title>
+	</head>
