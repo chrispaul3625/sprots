c3d6a7f1287df8c36c6b07a26d9aa9c7a8df6b3e
Chris Paul <chrispaul3625@gmail.com>
2016-03-12 11:43:17 -0700

    Edits to player service https://app.asana.com/0/84113647261172/94393680990027

--- public_html/angular/services/player-service.js
+++ public_html/angular/services/player-service.js
@@ -15,16 +15,4 @@ app.service("playerService", function($http, PLAYER_ENDPOINT) {
 	this.fetch = function(playerId) {
 		return($http.get(getUrlForId(playerId)));
 	};
-
-	this.create = function(player) {
-		return($http.post(getUrl(), player));
-	};
-
-	this.update = function(playerId, player) {
-		return($http.put(getUrlForId(playerId), player));
-	};
-
-	this.destroy = function(playerId) {
-		return($http.delete(getUrlForId(playerId)));
-	};
 });
