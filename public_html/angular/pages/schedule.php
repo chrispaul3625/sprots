a162de58fb4b13e5f408f976db127f6c751519c8
Michael Prinz <mnprinz@gmail.com>
2016-03-14 16:42:20 -0600

    added html to schedule page https://app.asana.com/0/84113659891109/94393680990011

--- public_html/angular/pages/schedule.php
+++ public_html/angular/pages/schedule.php
@@ -1,24 +1,24 @@
 	<div class="col-md-12">
 		<div class="alert alert-info" ng-if="players.length === 0">
-			<i class="fa fa-spinner fa-pulse" aria-hidden="true"></i> Loading players&hellip;
+			<i class="fa fa-spinner fa-pulse" aria-hidden="true"></i> Loading schedule&hellip;
 		</div>
 		<div class="content-box" ng-if="players.length > 0">
-			<h2> Player Stats</h2>
+			<h2>Game Schedule</h2>
 			<form class="form-inline inline pull-right" name="searchForm" id="searchForm">
 				<div class="form-group">
 					<label for="search">Search</label>
 					<div class="input-group">
-						<input type="text" class="form-control" ng-model="pagination.search" ng-change="switchPlayerArray();"/>
+						<input type="text" class="form-control" ng-model="pagination.search" ng-change="switchSchduleArray();"/>
 						<div class="input-group-addon">
 							<i class="fa fa-search" aria-hidden="true"></i>
 						</div>
 					</div>
 				</div>
 			</form>
-			<div ng-repeat="player in pagination.filteredPlayers | filter: pagination.search">
-				<p> Id {{ player.playerId }}
-					{{player.playerName}}
-					{{player.playerCity}}
+			<div ng-repeat="schedule in pagination.filteredPlayers | filter: pagination.search">
+				<p> Id {{ game.gameId }}
+					{{game.gameFirstTeamId}}
+					{{game.gameSecondTeamId}}
 				</p>
 			</div>
 			<uib-pagination
