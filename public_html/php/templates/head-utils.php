e149208c066fbd1a3f76041405126e69bf38bd8b
Domkratos <dmartinez337@cnm.edu>
2016-03-15 11:52:45 -0600

    re-arranged controllers, and services. added signup-controller/service

--- public_html/php/templates/head-utils.php
+++ public_html/php/templates/head-utils.php
@@ -50,17 +50,19 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/route-config.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/services/player-service.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/services/team-service.js"></script>
+		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/services/schedule-service.js"></script>
+		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/services/signup-service.js"></script>
+<!--		controllers here -->
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/home-controller.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/profile-controller.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/schedule-controller.js"></script>
-		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/schedule-service.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/sport-controller.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/player-controller.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/score-controller.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/position-controller.js"></script>
 		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/team-controller.js"></script>
-		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/login-controller.js"></script>
-		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/login-service.js"></script>
+		<script type="text/javascript" src="<?php echo $PREFIX;?>angular/controllers/singup-controller.js"></script>
+
 
 
 		<title><?php echo $PAGE_TITLE;?></title>
