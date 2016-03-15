9793214cbae2ac96cec02599610a7772828ba61e
Michael Prinz <mnprinz@gmail.com>
2016-03-15 11:21:36 -0600

    created login-service https://app.asana.com/0/84113659891109/94393680990011

---
+++ public_html/angular/pages/login.php
@@ -0,0 +1,4 @@
+<form class = "form-inline" id="loginForm" name="loginForm" ng-submit="ok();" novalidate>
+	<div class
+
+</form>
---
+++ public_html/angular/services/login-service.js
@@ -0,0 +1,9 @@
+app.constant("LOGIN_ENDPOINT", "php/api/login/");
+app.service("loginService", function($http, LOGIN_ENDPOINT) {
+	function getUrl() {
+		return(LOGIN_ENDPOINT);
+	}
+	this.login = function(login) {
+		return($http.post(getUrl(), login));
+	};
+});
