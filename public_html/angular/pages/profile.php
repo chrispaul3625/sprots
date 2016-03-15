e243420d012aad2c4624a03afe3a0dd32294bfc7
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-03-15 16:22:41 -0600

    add profile view to angular/pages/profile.php

---
+++ public_html/angular/pages/profile.php
@@ -0,0 +1,41 @@
+<div class="row">
+	<div class="col-xs-12">
+		<div class="jumbotron">
+			<h1>Profile</h1>
+			<p>Hello World!</p>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		<div class="content-box">
+			<h2> Favorite Sport League Standing Table</h2>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+			the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+			of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+			but also the leap into electronic typesetting, remaining essentially unchanged.
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="content-box">
+			<h2> Favorite Team Fixture Results for the Week </h2>
+			It is a long established fact that a reader will be distracted by the readable content of a page
+			when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+			distribution of letters, as opposed to using 'Content here, content here', making it look like
+			readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
+			their default model text, and a search for 'lorem ipsum' will uncover many web sites still in
+			their infancy.
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="content-box">
+			<h2> Favorite Player Statistic Updates </h2>
+			Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+			classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+			Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+			words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+			classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
+			and 1.10.33 of "de
+		</div>
+	</div>
+</div>
