3653a299095e949d013c46ceec3c69ca6505f211
Chris Paul <chrispaul3625@gmail.com>
2016-03-16 08:59:39 -0600

    Added Sprots Logo!
    https://app.asana.com/0/84113647261172/94393680990034

--- public_html/php/templates/header.php
+++ public_html/php/templates/header.php
@@ -11,7 +11,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index">Sprots Logo</a>
+					<img class="navbar-brand" src="images/sprotslogo.png"/>
 				</div>
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
