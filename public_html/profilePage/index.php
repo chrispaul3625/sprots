c463e19448e3cd80fa0d5c04c5b3329531be6be1
Michael Prinz <mnprinz@gmail.com>
2016-03-16 10:29:46 -0600

    Edited lorum ipsum https://app.asana.com/0/84113659891109/94393680990011

--- public_html/profilePage/index.php
+++ public_html/profilePage/index.php
@@ -53,32 +53,20 @@ require_once(dirname(__DIR__)."/php/templates/head-utils.php");
 						<div class="col-md-4">
 							<div class="content-box">
 								<h2> Favorite Sport League Standing Table</h2>
-								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-								the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-								of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-								but also the leap into electronic typesetting, remaining essentially unchanged.
+								Follow your favorite teams on Sprots!  We bring you the latest news and stats for your team and every player in the league!
+								If you like fantasy leagues, Sprots is for you, as you can research every player in the league and pick the best!
 						</div>
 				</div>
 						<div class="col-md-4">
 							<div class="content-box">
 								<h2> Favorite Team Fixture Results for the Week </h2>
-								It is a long established fact that a reader will be distracted by the readable content of a page
-								when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-								distribution of letters, as opposed to using 'Content here, content here', making it look like
-								readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
-								their default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-								their infancy.
+								Sprots delivers up-to-date sports news and statistics for your favorite teams and players.  If you're a sports fan, you'll love Sprots!
 							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="content-box">
 								<h2> Favorite Player Statistic Updates </h2>
-								Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-								classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-								Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
-								words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-								classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
-								and 1.10.33 of "de
+								Sprots is fast becoming the go to site for avid sports fans!  They know they find the best and most in-depth news and statistics about their favorite team!
 							</div>
 						</div>
 					</div>
@@ -96,13 +84,7 @@ require_once(dirname(__DIR__)."/php/templates/head-utils.php");
 			</div>
 		</div>
 		<h2> Footer </h2>
-		There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some
-		form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a
-		passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All
-		the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first
-		true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model
-		sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always
-		free from repetition, injected humour, or non-characteristic words etc
+		If you follow sports and have a fantasy team, you will love Sprots!  Follow your favorite player and teams!  Get the latest news and statistics for your favorite player!
 	</footer>
 
 </html>
